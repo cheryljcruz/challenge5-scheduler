c185dcb0e8833986868c38ff58f8c2a3cf7f0f96
koiclone <cheryljcruz1@gmail.com>
2022-03-10 12:57:54 -0600

    current day is displayed

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+// moment.js to  display current day at the top of calendar
+$("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
+console.log(currentDay);
+// time blocks for standard business hours
+// time blocks color coded to indicate whether past, present or future
+// click into time block to enter an event
+// click the save button for that time block > text for event is saved in localstorage
+// refresh page > saved events persist
