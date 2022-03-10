facfc13c254c925789738582c3662dd9560780a0
koiclone <cheryljcruz1@gmail.com>
2022-03-10 13:38:27 -0600

    added time blocks and corresponding elements in html

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@
 $("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
 console.log(currentDay);
 // time blocks for standard business hours
+// - columns inside rows
 // time blocks color coded to indicate whether past, present or future
 // click into time block to enter an event
 // click the save button for that time block > text for event is saved in localstorage
