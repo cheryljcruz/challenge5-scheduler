bb5375aba4b3893ea025c7a09dc50a174261451f
koiclone <cheryljcruz1@gmail.com>
2022-03-10 20:55:12 -0600

    function for color indicators, click function for saveBtn

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,35 @@
 // moment.js to  display current day at the top of calendar
 $("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
-console.log(currentDay);
 // time blocks for standard business hours
 // - columns inside rows
-
+var timeBlock = $(".time-block");
+var saveBtn = $(".saveBtn");
+var hourBlock = $(".hour");
+var textArea = $(".description");
 // time blocks color coded to indicate whether past, present or future
+
+function colorBlock() {
+    // get hours from current time
+    var setTime = moment().hours();
+$(timeBlock).each(function(){
+    var currentTime = parseInt($(this).attr("id"));
+
+    if (timeBlock > setTime) {
+        $(this).addClass(".future");
+    } else if 
+        (timeBlock === setTime) {
+            $(this).addClass(".present");
+        } else {
+            $(this).addClass(".past");
+
+        }
+    })
+};
+console.log(this);
 // click into time block to enter an event
 // click the save button for that time block > text for event is saved in localstorage
+saveBtn.on("click", function() {
+    
+})
+console.log(saveBtn);
 // refresh page > saved events persist
