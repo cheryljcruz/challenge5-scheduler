b9c01cc93bf30110ad77a71e06d68b52ff227cc1
koiclone <cheryljcruz1@gmail.com>
2022-03-11 22:04:56 -0600

    call for past present future & localstorage retrieval

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,55 @@
 // moment.js to  display current day at the top of calendar
 $("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
 // time blocks for standard business hours
-// - columns inside rows
-var timeBlock = $(".time-block");
-var saveBtn = $(".saveBtn");
-var hourBlock = $(".hour");
-var textArea = $(".description");
+// - columns inside rows - in html
 // time blocks color coded to indicate whether past, present or future
-
 function colorBlock() {
     // get hours from current time
-    var setTime = moment().hours();
-$(timeBlock).each(function(){
+    var setTime = moment().hour();
+ $(".time-block").each(function() {
     var currentTime = parseInt($(this).attr("id"));
 
-    if (timeBlock > setTime) {
-        $(this).addClass(".future");
+    if (currentTime > setTime) {
+        $(this).addClass("future");
+        $(this).removeClass("present");
+        $(this).removeClass("past");
     } else if 
-        (timeBlock === setTime) {
-            $(this).addClass(".present");
+        (currentTime === setTime) {
+            $(this).addClass("present");
+            $(this).removeClass("past");
+            $(this).removeClass("future");
         } else {
-            $(this).addClass(".past");
+            $(this).addClass("past");
+            $(this).removeClass("present");
+            $(this).removeClass("future");
 
         }
     })
 };
-console.log(this);
+colorBlock();
 // click into time block to enter an event
 // click the save button for that time block > text for event is saved in localstorage
+var saveBtn = $(".saveBtn");
+
 saveBtn.on("click", function() {
-    
-})
-console.log(saveBtn);
+    var textArea = $(this).siblings(".description").val();
+    var hourBlock = $(this).siblings(".hour").text();
+
+    localStorage.setItem(textArea, hourBlock);
+});
+
 // refresh page > saved events persist
+ function setBlock() {
+     $(".hour").each(function() {
+         var saveTime = $(this).text();
+         var saveText = localStorage.getItem(saveTime);
+         if (saveText !== null) {
+             $(this).siblings(".description").val(saveText);
+         }
+         
+     });
+     
+ }
+ 
+console.log(setBlock);
+setBlock();
